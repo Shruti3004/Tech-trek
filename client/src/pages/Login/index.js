e4eaaea2d1b754940c632323e9e1cfd39c9835e5
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-03-29 00:28:29 +0530

    feat: login screen
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="background">
+      <div className="flex justify-center items-center h-full">
+        <div className="blur-background  p-5">
+          <div className="login-container">
+            <div className="login-header">
+              <h1 className="font-bold main-heading text-white center">
+                Login
+              </h1>
+            </div>
+            <div className="login-body mt-6">
+              <form className="w-full max-w-xs">
+                <input className="w-full input-text" value="Shruti" />
+                {/* <input className="mt-4 w-1"/> */}
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
