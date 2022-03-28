a143f7b81a0bac9061986262a71ceb926bdbadc4
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-03-28 17:59:56 +0530

    feat: buttons
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- client/src/components/ButtonPrimary.jsx
+++ client/src/components/ButtonPrimary.jsx
@@ -4,7 +4,7 @@ const ButtonPrimary = ({ text, className, handleClick }) => {
   console.log(className);
   return (
     <div
-      className={`flex cursor-pointer hover:shadow-lg items-center px-9 py-5 justify-center button-background text-4xl text-primary font-bold ${
+      className={`flex cursor-pointer hover:shadow-lg items-center px-9 pt-5 pb-3 justify-center button-background text-4xl text-primary font-bold main-heading ${
         !!className && className
       }`}
       onClick={() => handleClick()}
