f8caffb69048d05ced5406e3b54e562c705091f4
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-03-28 17:56:40 +0530

    feat: login and signup pages
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes as Router, Route } from "react-router-dom";
 import App from "./App";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const Routes = () => {
   return (
@@ -16,10 +18,19 @@ const Routes = () => {
       />
       <Route
         exact
-        path="/register"
+        path="/login"
         element={
           <React.Suspense fallback={<div>Loading..</div>}>
-            <App />
+            <Login />
+          </React.Suspense>
+        }
+      />
+      <Route
+        exact
+        path="/signup"
+        element={
+          <React.Suspense fallback={<div>Loading..</div>}>
+            <Signup />
           </React.Suspense>
         }
       />
