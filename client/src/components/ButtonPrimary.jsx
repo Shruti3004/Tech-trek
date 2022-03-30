d0a8d3a7a262ed0effa472fd5cdd81ef62f536ca
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2022-03-31 02:29:56 +0530

    added font button

--- client/src/components/ButtonPrimary.jsx
+++ client/src/components/ButtonPrimary.jsx
@@ -5,9 +5,11 @@ const ButtonPrimary = ({ text, className, handleClick, disabled }) => {
     <button
       disabled={disabled}
       type="button"
-      className={`flex hover:shadow-lg items-center px-9 pt-5 pb-3 xs:pt-3 xs:pb-1 justify-center button-background text-4xl text-primary font-bold main-heading ${
+      className={`flex hover:shadow-lg items-center px-9 pt-5 pb-3 xs:pt-3 xs:pb-1 justify-center button-background text-4xl text-primary  main-heading ${
         className && className
-      } ${disabled ? "cursor-not-allowed" : "curson-pointer"}`}
+      } ${
+        disabled ? "cursor-not-allowed font-medium" : "curson-pointer font-bold"
+      }`}
       onClick={() => handleClick()}
     >
       {text}
