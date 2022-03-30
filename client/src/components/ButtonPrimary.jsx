afe4c1828c55a3467c94ebec5d1deaa89983c100
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-03-30 23:24:11 +0530

    fix: responsiveness
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- client/src/components/ButtonPrimary.jsx
+++ client/src/components/ButtonPrimary.jsx
@@ -5,7 +5,7 @@ const ButtonPrimary = ({ text, className, handleClick, disabled }) => {
     <button
       disabled={disabled}
       type="button"
-      className={`flex hover:shadow-lg items-center px-9 pt-5 pb-3 justify-center button-background text-4xl text-primary font-bold main-heading ${
+      className={`flex hover:shadow-lg items-center px-9 pt-5 pb-3 xs:pt-3 xs:pb-1 justify-center button-background text-4xl text-primary font-bold main-heading ${
         className && className
       } ${disabled ? "cursor-not-allowed" : "curson-pointer"}`}
       onClick={() => handleClick()}
