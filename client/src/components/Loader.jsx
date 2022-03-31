d0602b28c4f0a7da8f3f26d774fdb6dabdd729bc
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-03-31 14:13:18 +0530

    fix: loader
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <>
-      <img src="./loader.gif" height="200px" width="400px" />
-    </>
+    <div style={{ height: "100vh" }}>
+      <div className="flex justify-center align-center">
+        <img src="./loader.gif" height="120px" width="250px" />
+      </div>
+    </div>
   );
 };
 
