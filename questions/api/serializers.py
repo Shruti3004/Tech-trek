ecb0d6c1c0a176a498806501028df1741cfae060
OjusWiZard <ojuswimail@gmail.com>
2022-04-04 23:37:33 +0530

    fixed leaderboard
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- questions/api/serializers.py
+++ questions/api/serializers.py
@@ -23,16 +23,20 @@ class LeaderboardSerializer(serializers.BaseSerializer):
     def to_representation(self, obj):
         # badge_serializer = BadgeSerializer(badges, many=True)
         # badge4_count = obj.badges.filter(badge_type="4").count()
-        badges = obj.badges
+        badges = Badge.objects.filter(player=obj)
         special_badge = Badge.objects.get(badge_type="0")
-        if badges.contains(special_badge):
-            best_badge = special_badge
+        if special_badge in badges:
+            best_badge = special_badge.badge_type
         else:
-            best_badge = badges.order_by("-badge_type")[0]
+            best_badge = badges.order_by("-badge_type")
+            if best_badge.count() > 0:
+                best_badge = best_badge[0].badge_type
+            else:
+                best_badge = None
         return {
             "player_name": obj.username,
             "score": obj.score,
             "email": obj.email,
             "avatar_no": obj.avatar_no,
-            "best_badge": best_badge.badge_type,
+            "best_badge": best_badge,
         }
